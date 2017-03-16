cd4f1a7d691cea2c09d89907004915a27ba51cd8
PavelRoR <pavelvp2182@yandex.ru>
2017-03-17 02:25:13 +0500

    Верстка блока Преимущества

--- app/build/js/scripts.js
+++ app/build/js/scripts.js
@@ -6,5 +6,13 @@ $(function() {
     $('.close').click(function(e) {
         e.preventDefault();
         $('.lightbox_bg').fadeOut(500)
-    })
+    });
+});
+
+$(function() {
+    $("a[href^='#']").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
