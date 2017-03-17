585825d70bff2a80a6445a979f8c5d093ecc7695
PavelRoR <pavelvp2182@yandex.ru>
2017-03-17 14:05:28 +0500

    Начало верстки блока Фото

--- app/build/js/scripts.js
+++ app/build/js/scripts.js
@@ -10,6 +10,17 @@ $(function() {
 });
 
 $(function() {
+    $('body').on('click', '.pic_trigger', function(e) {
+        e.preventDefault();
+        $('#' + $(this).attr('data-id')).fadeIn(500);
+    });
+    $('.close').click(function(e) {
+        e.preventDefault();
+        $('.lightbox_bg').fadeOut(500)
+    });
+});
+
+$(function() {
     $("a[href^='#']").click(function() {
         var _href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
